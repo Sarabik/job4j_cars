8aaee4abe1c04921ced5886a958a37e69d947ec6
Sarabik <dalilija@inbox.lv>
2023-06-30 14:16:59 +0300

    3. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE auto_user
+	(id         serial primary key,
+	 login      varchar UNIQUE      NOT NULL,
+	 password   varchar             NOT NULL);
+
+CREATE TABLE auto_post
+	(id            serial primary key,
+	 description   varchar                       NOT NULL,
+	 created       boolean                       NOT NULL,
+	 auto_user_id  int REFERENCES auto_user (id) NOT NULL);
