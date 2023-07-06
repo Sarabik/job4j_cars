85afea9ad25d53dd9d2e669755869d7aea738cce
Sarabik <dalilija@inbox.lv>
2023-07-06 11:30:13 +0300

    2. HQL [#6874 #427960]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import ru.job4j.cars.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,12 +32,7 @@ public class UserRepository {
         Session session = sf.openSession();
         try {
             session.beginTransaction();
-            session.createQuery(
-                            "UPDATE User SET login = :fLogin, password = :fPassword WHERE id = :fId")
-                    .setParameter("fLogin", user.getLogin())
-                    .setParameter("fPassword", user.getPassword())
-                    .setParameter("fId", user.getId())
-                    .executeUpdate();
+            session.update(user);
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
@@ -62,37 +58,68 @@ public class UserRepository {
     }
 
     public List<User> findAllOrderById() {
+        List<User> list = new ArrayList<>();
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("from User order by id asc", User.class);
-        List<User> list = query.list();
-        session.close();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("from User order by id asc", User.class);
+            list = query.list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
         return list;
     }
 
     public Optional<User> findById(int userId) {
+        Optional<User> optionalUser = Optional.empty();
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("from User where id = :fId", User.class);
-        query.setParameter("fId", userId);
-        User user = query.uniqueResult();
-        session.close();
-        return Optional.ofNullable(user);
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("from User where id = :fId", User.class);
+            query.setParameter("fId", userId);
+            optionalUser = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return optionalUser;
     }
 
     public List<User> findByLikeLogin(String key) {
+        List<User> list = new ArrayList<>();
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("FROM User WHERE LOWER(login) LIKE LOWER(:fKey)", User.class);
-        query.setParameter("fKey", "%" + key + "%");
-        List<User> list = query.list();
-        session.close();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("FROM User WHERE LOWER(login) LIKE LOWER(:fKey)", User.class);
+            query.setParameter("fKey", "%" + key + "%");
+            list = query.list();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
         return list;
     }
 
     public Optional<User> findByLogin(String login) {
+        Optional<User> optionalUser = Optional.empty();
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("FROM User WHERE login = :fLogin", User.class);
-        query.setParameter("fLogin", login);
-        User user = query.uniqueResult();
-        session.close();
-        return Optional.ofNullable(user);
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("FROM User WHERE login = :fLogin", User.class);
+            query.setParameter("fLogin", login);
+            optionalUser = query.uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return optionalUser;
     }
 }
