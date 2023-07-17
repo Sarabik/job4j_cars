7c5d575857a6fe5f6d0b9e221e15bace74955307
Sarabik <dalilija@inbox.lv>
2023-07-17 16:26:13 +0300

    6. Лямбды и шаблон Command [#49295]

---
+++ src/main/java/ru/job4j/cars/repository/CrudRepository.java
@@ -0,0 +1,96 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@AllArgsConstructor
+public class CrudRepository {
+    private final SessionFactory sf;
+
+    /* Метод run для методов без возвращаемого значения (создание, обновление, удаление)
+    * Метод optional для методов возвращающих optional (поиск по значению одного из полей)
+    * Метод query для методов возвращающих коллекции */
+
+    public void run(Consumer<Session> command) {
+        tx(session -> {
+                    command.accept(session);
+                    return null;
+                }
+        );
+    }
+
+    public void run(String query, Map<String, Object> args) {
+        Consumer<Session> command = session -> {
+            Query<?> sq = session
+                    .createQuery(query);
+            for (Map.Entry<String, Object> arg : args.entrySet()) {
+                sq.setParameter(arg.getKey(), arg.getValue());
+            }
+            sq.executeUpdate();
+        };
+        run(command);
+    }
+
+    public <T> Optional<T> optional(String query, Class<T> cl, Map<String, Object> args) {
+        Function<Session, Optional<T>> command = session -> {
+            Query<T> sq = session
+                    .createQuery(query, cl);
+            for (Map.Entry<String, Object> arg : args.entrySet()) {
+                sq.setParameter(arg.getKey(), arg.getValue());
+            }
+            return sq.uniqueResultOptional();
+        };
+        return tx(command);
+    }
+
+    public <T> List<T> query(String query, Class<T> cl) {
+        Function<Session, List<T>> command = session -> session
+                .createQuery(query, cl)
+                .list();
+        return tx(command);
+    }
+
+    public <T> List<T> query(String query, Class<T> cl, Map<String, Object> args) {
+        Function<Session, List<T>> command = session -> {
+            Query<T> sq = session
+                    .createQuery(query, cl);
+            for (Map.Entry<String, Object> arg : args.entrySet()) {
+                sq.setParameter(arg.getKey(), arg.getValue());
+            }
+            return sq.list();
+        };
+        return tx(command);
+    }
+
+    /* Этот метод выполняет абстрактную операцию.
+    Он принимает некую "команду", открывает сессию, начинает транзакцию и выполняет эту команду.
+    Команда принимается в виде объекта функционального интерфейса, благодаря чему достигается абстрактность операции.
+    Методу tx() не важно, придет команда на вставку данных, изменение, удаление и т.д.
+    Он не знает её внутреннюю реализацию, он просто получает команду и выполняет её. */
+    public <T> T tx(Function<Session, T> command) {
+        Session session = sf.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            T rsl = command.apply(session);
+            transaction.commit();
+            return rsl;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+}
--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -8,118 +8,53 @@ import ru.job4j.cars.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @AllArgsConstructor
 public class UserRepository {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     public User create(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.persist(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.persist(user));
         return user;
     }
 
     public void update(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.update(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.merge(user));
     }
 
     public void delete(int userId) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.createQuery(
-                            "DELETE User WHERE id = :fId")
-                    .setParameter("fId", userId)
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(
+                "DELETE User WHERE id = :fId",
+                Map.of("fId", userId)
+        );
     }
 
     public List<User> findAllOrderById() {
-        List<User> list = new ArrayList<>();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("from User order by id asc", User.class);
-            list = query.list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return list;
+        return crudRepository.query(
+                "from User order by id asc", User.class
+        );
     }
 
     public Optional<User> findById(int userId) {
-        Optional<User> optionalUser = Optional.empty();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("from User where id = :fId", User.class);
-            query.setParameter("fId", userId);
-            optionalUser = query.uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return optionalUser;
+        return crudRepository.optional(
+                "from User where id = :fId", User.class,
+                Map.of("fId", userId)
+        );
     }
 
     public List<User> findByLikeLogin(String key) {
-        List<User> list = new ArrayList<>();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("FROM User WHERE LOWER(login) LIKE LOWER(:fKey)", User.class);
-            query.setParameter("fKey", "%" + key + "%");
-            list = query.list();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return list;
+        return crudRepository.query(
+                "FROM User WHERE LOWER(login) LIKE LOWER(:fKey)", User.class,
+                Map.of("fKey", "%" + key + "%")
+        );
     }
 
     public Optional<User> findByLogin(String login) {
-        Optional<User> optionalUser = Optional.empty();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("FROM User WHERE login = :fLogin", User.class);
-            query.setParameter("fLogin", login);
-            optionalUser = query.uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return optionalUser;
+        return crudRepository.optional(
+                "FROM User WHERE login = :fLogin", User.class,
+                Map.of("fLogin", login)
+        );
     }
 }
