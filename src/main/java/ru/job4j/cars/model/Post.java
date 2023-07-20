273bfdbfb866ed0268a9b15b18c86b1d360cec76
Sarabik <dalilija@inbox.lv>
2023-07-20 17:11:30 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -0,0 +1,26 @@
+package ru.job4j.cars.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "auto_post")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Post {
+    @Id
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private String description;
+
+    private boolean created;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "auto_post_id")
+    private List<PriceHistory> priceHistories = new ArrayList<>();
+}
