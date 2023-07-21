a94f648b0d905a301df057007f0ffe61d1b9ac5c
Sarabik <dalilija@inbox.lv>
2023-07-21 12:50:02 +0300

    1. ToMany [#301848]

--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -6,5 +6,5 @@ CREATE TABLE if not exists auto_user
 CREATE TABLE if not exists auto_post
 	(id            serial primary key,
 	 description   varchar                       NOT NULL,
-	 created       boolean                       NOT NULL,
+	 created       TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 	 auto_user_id  int REFERENCES auto_user (id) NOT NULL);
