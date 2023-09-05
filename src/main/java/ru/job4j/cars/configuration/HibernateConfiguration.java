a4939eb1317c39eabffddcd7337a0e9c7a1d1c9e
Sarabik <dalilija@inbox.lv>
2023-09-05 15:05:09 +0300

    2. Интеграционные тесты для проект job4j_cars [#4746 #436233]

--- src/main/java/ru/job4j/cars/configuration/HibernateConfiguration.java
+++ src/main/java/ru/job4j/cars/configuration/HibernateConfiguration.java
@@ -4,7 +4,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
